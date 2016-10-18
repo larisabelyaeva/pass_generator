9c10501a8e6ae4210c2014dfc74a275b3fd47009
larisabelyaeva <lg.belyaeva@yandex.ru>
2016-10-18 16:46:05 +0300

    Add files via upload

--- generator.rb
+++ generator.rb
@@ -1,47 +1,37 @@
-# генератор произносимых паролей
-# выводит заданное пользователем количество паролей заданной длины
+# pronounceable password generator
+# displays user-defined number of passwords
 
-$vowels = ["a", "e", "u", "o", "i", "y"]
-$consonants = ["q", "w", "r", "t", "p", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
+class PasswordGenerator
 
-# вспомогательные методы
-def is_vowel?(letter)
-  $vowels.include?(letter)
-end
+  public
 
-def is_consonant?(letter)
-  $consonants.include?(letter)
-end
-
-puts "Enter the number of passwords:"
-$number_of_passwords = gets.chomp!.to_i
-puts "Enter the password length (from 8 to 20):"
-$password_length = gets.chomp!.to_i
-while ($password_length < 8 || $password_length > 20)
-  puts "Please, enter number from 8 to 20"
-  $password_length = gets.chomp!.to_i
-end
-
-#метод, генерирующий пароли
-def create_passwords
-  passwords = Array.new($number_of_passwords)
-  i = 0
-  passwords.each { |password|
-    password = ($vowels + $consonants)[rand($vowels.length + $consonants.length - 1)]
-    pass_arr = password.split("")
-    $password_length.times do
-      if (is_vowel?(pass_arr[-1]))
-        pass_arr.push($consonants[rand($consonants.length - 1)])
-      else
-        pass_arr.push($vowels[rand($vowels.length - 1)])
-      end
+  # method of generating passwords
+  def create_passwords (number_of_passwords)
+    read_files
+    passwords = Array.new(number_of_passwords)
+    i = 0
+    passwords.each do |password|
+      password = @@prefixes[rand(@@prefixes.size- 1)]
+      password << @@syllables[rand(@@syllables.size - 1)]
+      password << @@postfixes[rand(@@postfixes.size - 1)]
+      passwords[i] = password
+      i += 1
     end
-    password = pass_arr.join("")
-    passwords[i] = password
-    i += 1
-  }
-  puts "Your passwords:"
-  passwords.each { |password| puts password  }
+    passwords
+  end
+
+  private
+
+  # this method reads files that contain syllables
+  def read_files
+    @@prefixes = IO.read('prefixes.txt').split(' ')
+    @@syllables = IO.read('syllables.txt').split(' ')
+    @@postfixes = IO.read('postfixes.txt').split(' ')
+  end
 end
 
-create_passwords
+puts 'Enter the number of passwords:'
+number_of_passwords = gets.chomp!.to_i
+
+generator = PasswordGenerator.new
+puts generator.create_passwords(number_of_passwords)
